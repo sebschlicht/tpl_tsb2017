c49133ea72c70a09d6bc5f9c36433d62f461051e
Sebastian Schlicht <sebastian@jablab.de>
2017-11-22 08:38:50 +0100

    module title overrides added

--- html/modules.php
+++ html/modules.php
@@ -18,5 +18,17 @@
     
     echo '</div>';
   }
+
+  function modChrome_sidebar_events( $module, &$params, &$attribs )
+  {
+    $module->title = JText::_( 'TPL_TSB2017_MOD_EVENTS' );
+    modChrome_sidebar( $module, $params, $attribs );
+  }
+
+  function modChrome_sidebar_partners( $module, &$params, &$attribs )
+  {
+    $module->title = JText::_( 'TPL_TSB2017_MOD_PARTNERS' );
+    modChrome_sidebar( $module, $params, $attribs );
+  }
 ?>
 
