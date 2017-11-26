14261a6a2bee120515e5fb34cc51d2b5bb254953
Sebastian Schlicht <sebastian@jablab.de>
2017-11-26 13:26:57 +0100

    use JSHint in Brackets

--- mkzip.sh
+++ mkzip.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 rm -f tsb2017.zip
-zip -q -9 -x "/.gitignore" -x "/.git/*" -x "/.idea/*" -x "/mkzip.sh" -x "/.mkzip.sh.swp" -x "/less/*" -r tsb2017.zip .
+zip -q -9 -x "/.gitignore" \
+  -x "/.git/*" \
+  -x "/.idea/*" \
+  -x "/mkzip.sh" \
+  -x "/.mkzip.sh.swp" \
+  -x "/.brackets.json" \
+  -x "/less/*" \
+  -r tsb2017.zip .
 
